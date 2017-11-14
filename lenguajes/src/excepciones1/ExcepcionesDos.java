1af08b895e789c9d5faf66d5105fa2d886a7c763
T-102 <T-102@PC265>
2017-11-13 19:38:13 -0600

    Sofia Jimena Melo Plaza Clase del 13/11/2017 :)

---
+++ lenguajes/src/excepciones1/ExcepcionesDos.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package excepciones1;
+
+/**
+ *
+ * @author T-102
+ */
+public class ExcepcionesDos {
+    public static void main(String[] h) {
+        //NumberFormatException.
+        String numerofalso="dos";
+        int valor=Integer.parseInt(numerofalso);
+        System.out.println(+valor);
+    }
+}
