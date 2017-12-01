988b14bde2a7c260577cf2eb86cc0c5e6872b6db
T-102 <T-102@PC265>
2017-11-30 19:37:26 -0600

    sofia imena melo plaza clase del 30-11-2017
    Insert y slect

--- lenguajes/src/conector/ProbarConexion1.java
+++ lenguajes/src/conector/ProbarConexion1.java
@@ -12,7 +12,7 @@ package conector;
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
- public class ProbarConexion {
+ public class ProbarConexion1 {
      public static void main(String[] args) {
          //declarar la conexion (variable con). Inicializarla null (valor de defecto)
          
@@ -22,31 +22,12 @@ import java.util.logging.Logger;
      con=Conexion.conectarse("root", "");
           System.out.println("Te conectaste!!");
           //Aqui vienen queries de mysql
+          //Caso2: select
+          //paso 1, generar una consulta (query)
+          Statement st= con.createStatement();
+          ResultSet rs=st.executeQuery("select*from tablita");
           
-          //Con la conexion que se llama con.
-          //vamos a generar una sentencia, la cual es una CLASE
-          //Statement st=con.createStatement();
           
-          //Generamos la tabla:
-          //st.execute("create table tablita(id integer primary key, nombre varchar(40))");
-             //System.out.println("Tabla generada con exito");
-             
-             //Caso 1: Insert
-             PreparedStatement st=con.prepareStatement("insert into tablita values(?,?");
-             
-             st.setInt(1, 1);
-             st.setString (2, "Jimena");
-             st.execute(); //Para ejecutar el statement
-            
-             
-             st.close();
-             System.out.println("Registro onsertado");
-             
-             // PreparedStatement st=con.prepareStatement("update tablita set nombre=? whereid=?);
-              //tambien se cierran las sentencias.
-             //al igual que las condiciones
-             st.close(); //metodo close
-             System.out.println("tabla cerrada con exito.");
        
          }catch(ClassNotFoundException e){
              System.out.println("NO se cargo bien el driver </3");
